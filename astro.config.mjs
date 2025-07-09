29ac63e1702140915346f48b5225b679b5b7b08a
Rafa <32463720+Rafacasari@users.noreply.github.com>
2025-07-09 12:11:04 -0300

    a lot of cool changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,11 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'ACNH Modding Wiki',
+			customCss: ['./src/styles/custom.css'],
 			social: [
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/acnhmodding/wiki' },
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/4cBd8dD6XS' }
+				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/4cBd8dD6XS' },
+				{ icon: 'reddit', label: 'Reddit', href: 'https://www.reddit.com/r/acnhmodding/' },
 			],
 			editLink: {
 				baseUrl: "https://github.com/acnhmodding/wiki/edit/main/"
@@ -32,6 +34,14 @@ export default defineConfig({
 				}),
 			],
 			sidebar: [
+				{
+					label: 'Getting Started',
+					autogenerate: { directory: "getting-started" },
+				},
+				{
+					label: 'Tools',
+					autogenerate: { directory: 'tools' },
+				},
 				{
 					label: 'Guides',
 					autogenerate: { directory: "guides" },
