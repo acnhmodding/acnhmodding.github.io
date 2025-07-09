1d55daf28f96d0898e15608f7af6fb13657d9773
Rafa <32463720+Rafacasari@users.noreply.github.com>
2025-07-09 10:52:19 -0300

    We exist!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,39 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator'
+import starlightThemeObsidian from 'starlight-theme-obsidian'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'ACNH Modding Wiki',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/acnhmodding/wiki' },
+				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/4cBd8dD6XS' }
+			],
+			editLink: {
+				baseUrl: "https://github.com/acnhmodding/wiki/edit/main/"
+			},
+			expressiveCode: {
+				themes: ['dracula', 'one-light'],
+			},
+			plugins: [
+				starlightLinksValidator(),
+				starlightThemeObsidian({
+					debug: false,
+					sitemapConfig: {},
+					graphConfig: {},
+					backlinksConfig: {},
+					graph: false,
+					backlinks: false
+				}),
+			],
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					autogenerate: { directory: "getting-started" },
 				},
 				{
 					label: 'Reference',
